13961bcf5f1dd655efbaca925bdddc0439254a15
Phellipe <phbrasiliano@gmail.com>
2017-06-04 18:18:28 +0200

    starts the form for new user, and changes its controler and model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
-  
+
 end
